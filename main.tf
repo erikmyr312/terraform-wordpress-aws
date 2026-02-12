2848ccc9716486ca09f24e4564d9fa686bfff939
erikmyr312 <erik.myr@edu.312school.com>
2026-02-12 13:40:51 -0600

    VPC + subnets + route tale and security group created

--- main.tf
+++ main.tf
@@ -28,17 +28,122 @@ resource "aws_internet_gateway" "wordpress_igw" {
 ############################################
 # Public Route Table
 # Routes outbound traffic to Internet Gateway
+# IMPORTANT: Only PUBLIC subnets should be associated to this RT
 ############################################
 
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "wordpress_rt" {
   vpc_id = aws_vpc.wordpress_vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.wordpress_igw.id
+  tags = {
+    Name = "wordpess-rt"
+  }
+}
+
+resource "aws_route" "default_internet_route" {
+  route_table_id         = aws_route_table.wordpress_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.wordpress_igw.id
+}
+
+# ------------------------------------------------------------
+# Public Subnets
+# - map_public_ip_on_launch = true so EC2 gets a public IP
+# - spread across 3 AZs for high availability pattern
+# ------------------------------------------------------------
+resource "aws_subnet" "public" {
+  count                   = 3
+  vpc_id                  = aws_vpc.wordpress_vpc.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "wordpress-public-${count.index + 1}"
+  }
+}
+
+# ------------------------------------------------------------
+# Private Subnets 
+# - used for RDS
+# - no direct internet route table association
+# ------------------------------------------------------------
+resource "aws_subnet" "private" {
+  count             = 3
+  vpc_id            = aws_vpc.wordpress_vpc.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
+  tags = {
+    Name = "wordpress-private-${count.index + 1}"
+  }
+}
+
+# ------------------------------------------------------------
+# Associate ONLY public subnets to the public route table
+# Private subnets stay private (no IGW route)
+# ------------------------------------------------------------
+resource "aws_route_table_association" "public_assoc" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.wordpress_rt.id
+}
+
+############################################
+# WordPress Security Group
+# Allows HTTP, HTTPS, SSH from Internet
+############################################
+
+resource "aws_security_group" "wordpress_sg" {
+  name        = "wordpress-sg"
+  description = "Allow web and SSH traffic"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  dynamic "ingress" {
+    for_each = var.ingress_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "wordpress-public-rt"
+    Name = "wordpress-sg"
+  }
+}
+############################################
+# RDS Security Group
+# Allows MySQL ONLY from WordPress SG
+############################################
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow MySQL from WordPress"
+  vpc_id      = aws_vpc.wordpress_vpc.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.wordpress_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "rds-sg"
   }
 }
--- variables.tf
+++ variables.tf
@@ -9,6 +9,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+# ---------------------------------------------
+# Networking (VPC + Subnets)
+# ---------------------------------------------
 
 variable "vpc_cidr" {
   description = "CIDR block for WordPress VPC"
@@ -16,13 +19,38 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
+# 3 AZs in us-east-1
+variable "azs" {
+  description = "Availability Zones for subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+# Public subnets (one per AZ)
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+# Private subnets (one per AZ)
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+}
+
+
+# Ports allowed for WordPress EC2
 variable "ingress_ports" {
-  description = "List of allowed inbound ports for WordPress EC2"
+  description = "Ports allowed from the internet to WordPress instance"
   type        = list(number)
   default     = [22, 80, 443]
 }
 
-variable "ami_id" {
-  description = "Amazon Linux 2 AMI ID"
-  type        = string
-}
+
+
+# variable "ami_id" {
+#   description = "Amazon Linux 2 AMI ID"
+#   type        = string
+# }
